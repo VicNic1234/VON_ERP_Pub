e738048a446c79c2352e54e19ff512ecced6c4be
Victor Okonkwo <victor_oko2006@yahoo.com>
2024-02-24 11:55:15 +0000

    Updated 404 page

---
+++ 404.php
@@ -0,0 +1,46 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>VON ERP | 404 </title>
+	<link rel="icon" href="mBoot/plant.png" type="image/png" sizes="10x10">
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <!-- Bootstrap 3.3.4 -->
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	
+    <!-- Font Awesome Icons -->
+    <link href="../mBOOT/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- Ionicons -->
+    <link href="../mBOOT/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <!-- jvectormap -->
+    <link href="../plugins/jvectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="../dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link href="../dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
+  </head>
+  <body class="skin-blue sidebar-mini">
+  
+        <!-- Main content -->
+        <section class="content">
+          <!-- Info boxes -->
+          <div>404</div>
+        </section>
+         
+
+  </body>
+</html>
